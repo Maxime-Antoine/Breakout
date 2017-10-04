b220d163e331e8771c576f8bba10d3a87abb822c
Maxime <maxime.antoine2@gmail.com>
2017-10-04 19:35:10 +0800

    Added Ball script

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float initialVelocity = 600f;
+
+    private Rigidbody rigidbody;
+    private bool ballInPlay = false;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetButtonDown("Fire1") && !ballInPlay)
+        {
+            transform.parent = null;
+            ballInPlay = true;
+            rigidbody.isKinematic = false;
+            rigidbody.AddForce(new Vector3(initialVelocity, initialVelocity, 0));
+        }
+	}
+}
